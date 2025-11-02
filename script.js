3f338d4f78d8b20cded1983f668ae2d2cfea3ea5
ralfandreas <ralf.andreas.vendel@ut.ee>
2025-11-02 14:38:16 +0200

    some comments

--- script.js
+++ script.js
@@ -2,31 +2,31 @@
  /*
   const apiUrl = "https://api.jsonbin.io/v3/b/6907238f43b1c97be99371ba/latest";
 
-  fetch(apiUrl)
+  fetch(apiUrl) //get the JSON data from apiUrl
     .then(response => {
-      return response.json();
+      return response.json();//converts the response into a usable JSON object.
     })
     .then(data => {
-      const posts = data.record.Posts;
-      const container = document.getElementById("posts-container");
+      const posts = data.record.Posts; // JSONBin puts your file inside an extra “record” layer so we use data.record.Posts
+      const container = document.getElementById("posts-container");//finds the "posts-container"
 
-      container.innerHTML = "";
+      container.innerHTML = "";//clears that area
 
-      posts.forEach(post => {
-        const postDiv = document.createElement("div");
-        postDiv.classList.add("postitus");
+      posts.forEach(post => { // loops through every post in the JSON
+        const postDiv = document.createElement("div");// creates a new <div> for each one
+        postDiv.classList.add("postitus");// ads postitus class for css.
 
-        postDiv.innerHTML = `
-          <img src="${post.profileImage}" alt="User" class="pildike">
+        postDiv.innerHTML =  // fills that <div>
+          <img src="${post.profileImage}" alt="User" class="pildike"> //${} to insert real data from your JSON.
           <p class="parem">${post.date}</p>
-          ${post.image ? `<img src="${post.image}" alt="Post image" class="pildike2">` : ""}
+          ${post.image ? <img src="${post.image}" alt="Post image" class="pildike2"> : ""}
           <p class="vasak"><strong>${post.author}:</strong> ${post.content}</p>
-        `;
+        ;
 
-        container.appendChild(postDiv);
+        container.appendChild(postDiv);// adds each post to webpage
       });
     })
-    .catch(error => console.error("Error fetching posts:", error));
+    .catch(error => console.error("Error fetching posts:", error));// throws and error if anyhing goes wrong.
 */
 
 
